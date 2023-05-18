df6e5bd94d273bb53176a097d4478478603f019d
technoY2K <cloud@kevia.me>
2023-05-17 18:35:44 -0700

    chore: config for aws lambda

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
-})
+    nitro: {
+        preset: "aws-lambda",
+    },
+    app: {
+        cdnURL: "https://d302kctrc8u730.cloudfront.net",
+    },
+});
