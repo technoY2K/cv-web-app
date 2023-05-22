571e5d5472ab27dd0ca8777d4682abea9fb4caaa
technoY2K <cloud@kevia.me>
2023-05-21 20:33:23 -0700

    chore: save point

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,15 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    nitro: {
-        preset: "aws-lambda",
-    },
     app: {
         cdnURL: "https://d302kctrc8u730.cloudfront.net",
     },
-    modules: ["@nuxtjs/tailwindcss", "@nuxtjs/google-fonts"],
     googleFonts: {
         families: {
             Roboto: true,
         },
+    },
+    modules: ["@nuxtjs/tailwindcss", "@nuxtjs/google-fonts"],
+    nitro: {
+        preset: "aws-lambda",
     },
 });
