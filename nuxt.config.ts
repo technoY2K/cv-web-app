e8b0897881354813285750e7caf6e3eb6e11cde0
technoY2K <cloud@kevia.me>
2023-06-09 16:59:29 -0700

    chore: minor changes to initial set up

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,15 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    app: {
-        cdnURL: "https://d302kctrc8u730.cloudfront.net",
-    },
     googleFonts: {
         families: {
             Roboto: true,
         },
     },
     modules: ["@nuxtjs/tailwindcss", "@nuxtjs/google-fonts"],
-    nitro: {
-        preset: "aws-lambda",
-    },
 });
--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,11 @@
 <template>
-    <div class="card bg-base-100 w-96 p-6 shadow-xl">
+    <div class="card w-96 bg-base-100 p-6 shadow-xl">
         <section class="card-body">
             <h2 class="card-title">Counter</h2>
             <p>{{ counter }}</p>
         </section>
         <section class="card-actions justify-end">
-            <button class="btn btn-primary" @click="increment">Click Me</button>
+            <button class="btn-primary btn" @click="increment">Click Me</button>
         </section>
     </div>
 </template>
