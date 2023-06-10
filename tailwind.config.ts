a1c3c6e15fd85ed968a3b8a3b1042dc4647b9a77
technoY2K <cloud@kevia.me>
2023-06-09 17:19:06 -0700

    feat: add baseline header sizes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,20 @@
 import defaultTheme from "tailwindcss/defaultTheme";
+import plugin from "tailwindcss/plugin";
 
 module.exports = {
-    plugins: [require("daisyui")],
+    plugins: [
+        require("daisyui"),
+        plugin(function ({ addBase, theme }) {
+            addBase({
+                h1: { fontSize: theme("fontSize.5xl") },
+                h2: { fontSize: theme("fontSize.4xl") },
+                h3: { fontSize: theme("fontSize.3xl") },
+                h4: { fontSize: theme("fontSize.2xl") },
+                h5: { fontSize: theme("fontSize.xl") },
+                h6: { fontSize: theme("fontSize.lg") },
+            });
+        }),
+    ],
     theme: {
         extend: {
             fontFamily: {
